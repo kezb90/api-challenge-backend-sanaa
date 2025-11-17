5147079b84e26d1353afad21f8c2efbfe7770c5c
Mohsen Rahmati <m90.rahmati@gmail.com>
2025-11-17 14:11:17 +0330

    تکمیل api مربوط document

--- dms_with_drf/document/urls.py
+++ dms_with_drf/document/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DocumentViewSet
 
-urlpatterns = [
-    
-]
+router = DefaultRouter()
+router.register(r"documents", DocumentViewSet, basename="document")
+
+urlpatterns = router.urls
--- dms_with_drf/document/views.py
+++ dms_with_drf/document/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from .permissions import DocumentPermission
+from .serializers import DocumentSerializer
+from .models import Document
 
-# Create your views here.
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all().order_by("-uploaded_at")
+    serializer_class = DocumentSerializer
+    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [DocumentPermission]
+
+    def perform_create(self, serializer):
+        serializer.save(uploaded_by=self.request.user)
