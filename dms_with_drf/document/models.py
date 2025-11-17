8c5382c8439060e791663c227e8aced02c861015
Mohsen Rahmati <m90.rahmati@gmail.com>
2025-11-17 11:10:37 +0330

    تعریف مدل document

--- dms_with_drf/document/models.py
+++ dms_with_drf/document/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
