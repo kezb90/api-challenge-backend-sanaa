a6385db471e8fbba9a94c312432faaf2541d0c39
Mohsen Rahmati <m90.rahmati@gmail.com>
2025-11-17 11:10:08 +0330

    تنظیم مسیر static & media در settings.py and root url

--- dms_with_drf/dms_with_drf/urls.py
+++ dms_with_drf/dms_with_drf/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -36,5 +37,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('accounts.urls')),
+    path('document/', include('document.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
