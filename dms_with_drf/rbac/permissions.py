20b3259714e395b63ddcc88a9c51d17874d8310d
Mohsen Rahmati <m90.rahmati@gmail.com>
2025-11-19 11:40:12 +0330

    خواناتر نوشتن کد permission rbca

--- dms_with_drf/rbac/permissions.py
+++ dms_with_drf/rbac/permissions.py
@@ -1,15 +1,25 @@
 from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission):
+    """
+    Allow only users in 'admin' group OR Django superusers.
+    """
+
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_superuser or request.user.groups.filter(name='admin').exists()
-        )
+        user = request.user
+
+        if not user or not user.is_authenticated:
+            return False
+        
+        if user.is_superuser:
+            return True
+
+        return user.groups.filter(name="admin").exists()
 
 class IsEditor(permissions.BasePermission):
-    def has_permission(self, request, view):
+    def has_permission(self, request):
         return request.user.is_authenticated and request.user.groups.filter(name__in=['admin', 'editor']).exists()
 
 class IsViewer(permissions.BasePermission):
-    def has_permission(self, request, view):
+    def has_permission(self, request):
         return request.user.is_authenticated
